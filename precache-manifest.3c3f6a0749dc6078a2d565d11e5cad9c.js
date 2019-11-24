b82f3b58bdbb83ac30e0dce6585f0b83efed68c2
Pan Liu <panliu@lyft.com>
2019-11-24 14:39:17 -0800

    Updates

---
+++ precache-manifest.3c3f6a0749dc6078a2d565d11e5cad9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc5ba52591ef2a14c9d10c6e9a349d8",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a84802d18adb318150eb",
+    "url": "/monster-rolodex/static/css/main.36e4034e.chunk.css"
+  },
+  {
+    "revision": "c6214f979b742b0bcadf",
+    "url": "/monster-rolodex/static/js/2.f65d83a6.chunk.js"
+  },
+  {
+    "revision": "a84802d18adb318150eb",
+    "url": "/monster-rolodex/static/js/main.f3e62794.chunk.js"
+  },
+  {
+    "revision": "78872b30b8d5e18a4390",
+    "url": "/monster-rolodex/static/js/runtime-main.716ed2c9.js"
+  }
+]);
